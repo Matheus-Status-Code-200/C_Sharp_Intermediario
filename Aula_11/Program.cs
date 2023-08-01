f727e6795064f58310eeed5a597f0415c9fdbfe1
Matheus-Status-Code-200 <testeselenium600@gmail.com>
2023-08-01 19:42:51 -0300

    Multiplicação usando Delegate.

--- Aula_11/Program.cs
+++ Aula_11/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.ForegroundColor = ConsoleColor.Cyan;
+        Console.WriteLine("Cálculo de Multiplicação usando Delegate");
+        Console.WriteLine("----------------------------------------\n");
+        Console.WriteLine("Nesse programa, usamos delegate para realizar a operação de multiplicação.");
+        Console.WriteLine("O delegate é uma referência a uma função anônima que executa a multiplicação de dois números.\n");
+
+        var multiplicacao = delegate(double x, double y) { return x * y; };
+        double num1 = 20;
+        double num2 = 30;
+        double resultado = multiplicacao(num1, num2);
+        Console.ResetColor();
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine($"Resultado da Multiplicação de {num1} por {num2}: {resultado}");
+
+        Console.WriteLine("\nMais Projetos: https://github.com/Matheus-Status-Code-200\n");
+        Console.ResetColor();
+    }
+}
