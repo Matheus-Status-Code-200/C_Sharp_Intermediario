40dfdfab707333ff793f0d261756e11ee03af7af
Matheus-Status-Code-200 <testeselenium600@gmail.com>
2023-08-01 19:59:08 -0300

    Delegate para criar uma função e verificar  nome fornecido.

---
+++ Aula_12/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.ForegroundColor = ConsoleColor.Cyan;
+        Console.WriteLine("Exemplo de Delegate em C#");
+        Console.WriteLine("-------------------------\n");
+        Console.WriteLine("Nesse programa, usamos delegate para criar uma função anônima que recebe");
+        Console.WriteLine("um nome como entrada e imprime uma saudação. Também usamos delegate para");
+        Console.WriteLine("verificar se o nome fornecido é 'Matheus', ignorando a sensibilidade de letras.\n");
+
+        var getName = delegate(string name)
+        {
+            Console.WriteLine($"Olá {name}");
+        };
+
+        Console.Write("Digite seu nome: ");
+        var addName = Console.ReadLine();
+        getName(addName);
+
+        Func<string, bool> checkedName = delegate(string name)
+        {
+            return string.Equals(name, "Matheus", StringComparison.OrdinalIgnoreCase);
+        };
+
+        Console.WriteLine(checkedName(addName));
+
+        Console.WriteLine("\nMais Projetos: https://github.com/Matheus-Status-Code-200\n");
+        Console.ResetColor();
+    }
+}
