a493595a4c61b56449facfe7b029d4a706843026
Matheus-Status-Code-200 <testeselenium600@gmail.com>
2023-08-04 19:07:41 -0300

    Introdução a Namespaces.

---
+++ Aula_14/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ExemploNamespace
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Programa 1 - Introdução a Namespaces");
+            Console.WriteLine("-----------------------------------");
+            Console.WriteLine("Um namespace é um mecanismo de organização de código em C#.");
+            Console.WriteLine("Ele permite agrupar classes, interfaces e outros membros em uma hierarquia lógica.");
+            Console.WriteLine("Isso ajuda a evitar conflitos de nomes entre diferentes partes do código.");
+            Console.WriteLine("Namespaces também permitem que você acesse classes de bibliotecas e frameworks.");
+        }
+    }
+}
