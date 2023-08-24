aad79a4c3c83c0be6213cf620b51dd96c6f73baf
Matheus-Status-Code-200 <testeselenium600@gmail.com>
2023-08-24 16:54:36 -0300

    Introdução a POO

--- Aula_24/Program.cs
+++ Aula_24/Program.cs
@@ -1,61 +1,57 @@
 ﻿using System;
 
-class Carro
-{
-    public string Modelo { get; set; }
-    public string Cor { get; set; }
-    public int NumeroPortas { get; set; }
-    public string TipoMotor { get; set; }
-    public string TipoCambio { get; set; }
-
-    public void ExibirDetalhes()
-    {
-        Console.WriteLine($"Detalhes do Carro:");
-        Console.WriteLine($"Modelo: {Modelo}");
-        Console.WriteLine($"Cor: {Cor}");
-        Console.WriteLine($"Número de Portas: {NumeroPortas}");
-        Console.WriteLine($"Tipo de Motor: {TipoMotor}");
-        Console.WriteLine($"Tipo de Câmbio: {TipoCambio}");
-    }
-}
-
 class Program
 {
+    class Animal
+    {
+        public string Nome { get; set; }
+        public int Idade { get; set; }
+
+        public void EmitirSom()
+        {
+            Console.WriteLine("O animal emitiu um som.");
+        }
+    }
+
+    class Cachorro : Animal
+    {
+        public void Latir()
+        {
+            Console.WriteLine("O cachorro está latindo.");
+        }
+    }
+
+    class Gato : Animal
+    {
+        public void Miar()
+        {
+            Console.WriteLine("O gato está miando.");
+        }
+    }
+
     static void Main()
     {
         Console.ForegroundColor = ConsoleColor.Cyan;
-        Console.WriteLine("Programa de Detalhes de Carro");
-        Console.WriteLine("-----------------------------\n");
-        Console.WriteLine("Neste programa, você pode informar os detalhes de um carro.\n");
+        Console.WriteLine("Programa de Demonstração de POO");
+        Console.WriteLine("--------------------------------\n");
+        Console.WriteLine("Neste programa, ilustramos alguns conceitos de Programação Orientada a Objetos.\n");
         Console.ResetColor();
 
-        Carro carro = new Carro();
+        Cachorro cachorro = new Cachorro();
+        cachorro.Nome = "Rex";
+        cachorro.Idade = 3;
 
-        Console.Write("Digite o modelo do carro (Modelo 1, Modelo 2, Modelo 3): ");
-        carro.Modelo = Console.ReadLine();
+        Gato gato = new Gato();
+        gato.Nome = "Miau";
+        gato.Idade = 2;
 
-        Console.Write("Digite a cor do carro: ");
-        carro.Cor = Console.ReadLine();
+        Console.WriteLine($"Cachorro: {cachorro.Nome}, {cachorro.Idade} anos");
+        cachorro.Latir();
+        cachorro.EmitirSom();
 
-        Console.Write("Digite o número de portas do carro: ");
-        if (int.TryParse(Console.ReadLine(), out int numeroPortas))
-        {
-            carro.NumeroPortas = numeroPortas;
-        }
-        else
-        {
-            Console.WriteLine("Número de portas inválido. Usando valor padrão (4).");
-            carro.NumeroPortas = 4;
-        }
-
-        Console.Write("Digite o tipo de motor do carro: ");
-        carro.TipoMotor = Console.ReadLine();
-
-        Console.Write("Digite o tipo de câmbio do carro: ");
-        carro.TipoCambio = Console.ReadLine();
-
-        Console.WriteLine();
-        carro.ExibirDetalhes();
+        Console.WriteLine($"\nGato: {gato.Nome}, {gato.Idade} anos");
+        gato.Miar();
+        gato.EmitirSom();
 
         Console.ForegroundColor = ConsoleColor.Green;
         Console.WriteLine("\nMais Projetos e Exemplos em: https://github.com/Matheus-Status-Code-200\n");
---
+++ Aula_25/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+class Carro
+{
+    public string Modelo { get; set; }
+    public string Cor { get; set; }
+    public int NumeroPortas { get; set; }
+    public string TipoMotor { get; set; }
+    public string TipoCambio { get; set; }
+
+    public void ExibirDetalhes()
+    {
+        Console.WriteLine($"Detalhes do Carro:");
+        Console.WriteLine($"Modelo: {Modelo}");
+        Console.WriteLine($"Cor: {Cor}");
+        Console.WriteLine($"Número de Portas: {NumeroPortas}");
+        Console.WriteLine($"Tipo de Motor: {TipoMotor}");
+        Console.WriteLine($"Tipo de Câmbio: {TipoCambio}");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Console.ForegroundColor = ConsoleColor.Cyan;
+        Console.WriteLine("Programa de Detalhes de Carro");
+        Console.WriteLine("-----------------------------\n");
+        Console.WriteLine("Neste programa, você pode informar os detalhes de um carro.\n");
+        Console.ResetColor();
+
+        Carro carro = new Carro();
+
+        Console.Write("Digite o modelo do carro (Modelo 1, Modelo 2, Modelo 3): ");
+        carro.Modelo = Console.ReadLine();
+
+        Console.Write("Digite a cor do carro: ");
+        carro.Cor = Console.ReadLine();
+
+        Console.Write("Digite o número de portas do carro: ");
+        if (int.TryParse(Console.ReadLine(), out int numeroPortas))
+        {
+            carro.NumeroPortas = numeroPortas;
+        }
+        else
+        {
+            Console.WriteLine("Número de portas inválido. Usando valor padrão (4).");
+            carro.NumeroPortas = 4;
+        }
+
+        Console.Write("Digite o tipo de motor do carro: ");
+        carro.TipoMotor = Console.ReadLine();
+
+        Console.Write("Digite o tipo de câmbio do carro: ");
+        carro.TipoCambio = Console.ReadLine();
+
+        Console.WriteLine();
+        carro.ExibirDetalhes();
+
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine("\nMais Projetos e Exemplos em: https://github.com/Matheus-Status-Code-200\n");
+        Console.ResetColor();
+    }
+}
